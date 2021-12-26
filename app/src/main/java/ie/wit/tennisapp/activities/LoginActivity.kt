b7a92a78d93741d7f10fece5d1b35ea2d50c071b
Tony Naughton <tonynaughton1@gmail.com>
2021-12-26 21:21:21 +0000

    Implemented basic firebase authentication

--- app/src/main/java/ie/wit/tennisapp/activities/LoginActivity.kt
+++ app/src/main/java/ie/wit/tennisapp/activities/LoginActivity.kt
@@ -1,28 +1,38 @@
 package ie.wit.tennisapp.activities
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import ie.wit.tennisapp.R
 import ie.wit.tennisapp.databinding.ActivityLoginBinding
 import ie.wit.tennisapp.fragments.ResultsFragment
+import ie.wit.tennisapp.helpers.showImagePicker
 import ie.wit.tennisapp.main.MainApp
 import ie.wit.tennisapp.models.MemberModel
+import timber.log.Timber
 
-class LoginActivity() : AppCompatActivity() {
+class LoginActivity() : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityLoginBinding
     var member = MemberModel()
     lateinit var app: MainApp
+    private lateinit var auth: FirebaseAuth
+    private lateinit var togglePasswordVisButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,44 +44,99 @@ class LoginActivity() : AppCompatActivity() {
 
         app = application as MainApp
 
-        val togglePasswordVisButton = findViewById<ImageButton>(R.id.togglePasswordVisibility)
+        binding.loginButton.setOnClickListener(this)
+        binding.togglePasswordVisButton.setOnClickListener(this)
+
+//        binding.loginButton.setOnClickListener() {
+//            member.email = binding.memberEmail.text.toString()
+//            member.password = binding.memberPassword.text.toString()
+//            if (member.email.isEmpty() || member.password.isEmpty()) {
+//                Snackbar
+//                    .make(it, R.string.fill_in_all_fields, Snackbar.LENGTH_LONG)
+//                    .show()
+//            } else {
+//                var verifiedUser = false
+//                var allMembers = app.members.findAll()
+//                allMembers.forEach { it ->
+//                    if ((it.email == member.email) && (it.password == member.password)) {
+//                        verifiedUser = true
+//                    }
+//                }
+//                if (verifiedUser) {
+//                    setResult(RESULT_OK)
+//                    startActivity(Intent(this, ResultsFragment::class.java))
+//                } else {
+//                    Snackbar
+//                        .make(it, R.string.user_does_not_exist, Snackbar.LENGTH_LONG)
+//                        .show()
+//                }
+//            }
+//        }
+
+        togglePasswordVisButton = findViewById(R.id.togglePasswordVisButton)
         togglePasswordVisButton.setImageResource(R.drawable.ic_eye)
 
-        binding.togglePasswordVisibility.setOnClickListener() {
-            val passwordEntry = findViewById<EditText>(R.id.memberPassword)
-            if(togglePasswordVisButton.drawable.constantState == ContextCompat.getDrawable(this, R.drawable.ic_eye)?.constantState) {
-                togglePasswordVisButton.setImageResource(R.drawable.ic_eye_slash)
-                passwordEntry.transformationMethod = HideReturnsTransformationMethod.getInstance()
-            } else {
-                togglePasswordVisButton.setImageResource(R.drawable.ic_eye)
-                passwordEntry.transformationMethod = PasswordTransformationMethod.getInstance()
-            }
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun logIn(email: String, password: String) {
+        Timber.d( "signIn:$email")
+        if (!validateForm()) {
+            return
         }
 
-        binding.btnAdd.setOnClickListener() {
-            member.email = binding.memberEmail.text.toString()
-            member.password = binding.memberPassword.text.toString()
-            if (member.email.isEmpty() || member.password.isEmpty()) {
-                Snackbar
-                    .make(it, R.string.fill_in_all_fields, Snackbar.LENGTH_LONG)
-                    .show()
-            } else {
-                var verifiedUser = false
-                var allMembers = app.members.findAll()
-                allMembers.forEach { it ->
-                    if ((it.email == member.email) && (it.password == member.password)) {
-                        verifiedUser = true
-                    }
-                }
-                if (verifiedUser) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Timber.d( "signInWithEmail:success")
+                    val user = auth.currentUser
                     setResult(RESULT_OK)
-                    startActivity(Intent(this, ResultsFragment::class.java))
+                    startActivity(Intent(this, Home::class.java))
                 } else {
-                    Snackbar
-                        .make(it, R.string.user_does_not_exist, Snackbar.LENGTH_LONG)
-                        .show()
+                    // If sign in fails, display a message to the user.
+                    Timber.w( "signInWithEmail:failure $task.exception")
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+                if (!task.isSuccessful) {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = binding.memberEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.memberEmail.error = "Required."
+            valid = false
+        } else {
+            binding.memberEmail.error = null
+        }
+
+        val password = binding.memberPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.memberPassword.error = "Required."
+            valid = false
+        } else {
+            binding.memberPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun togglePasswordVisibility() {
+        val passwordEntry = findViewById<EditText>(R.id.memberPassword)
+        if(togglePasswordVisButton.drawable.constantState == ContextCompat.getDrawable(this, R.drawable.ic_eye)?.constantState) {
+            togglePasswordVisButton.setImageResource(R.drawable.ic_eye_slash)
+            passwordEntry.transformationMethod = HideReturnsTransformationMethod.getInstance()
+        } else {
+            togglePasswordVisButton.setImageResource(R.drawable.ic_eye)
+            passwordEntry.transformationMethod = PasswordTransformationMethod.getInstance()
         }
     }
 
@@ -87,6 +152,13 @@ class LoginActivity() : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.loginButton -> logIn(binding.memberEmail.text.toString(), binding.memberPassword.text.toString())
+            R.id.togglePasswordVisButton -> togglePasswordVisibility()
+        }
     }
 }
 
--- app/src/main/java/ie/wit/tennisapp/activities/RegisterActivity.kt
+++ app/src/main/java/ie/wit/tennisapp/activities/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.view.Menu
@@ -14,20 +15,25 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.squareup.picasso.Picasso
 import ie.wit.tennisapp.R
 import ie.wit.tennisapp.databinding.ActivityRegisterBinding
 import ie.wit.tennisapp.helpers.showImagePicker
 import ie.wit.tennisapp.main.MainApp
 import ie.wit.tennisapp.models.MemberModel
+import timber.log.Timber
 import timber.log.Timber.i
 
-class RegisterActivity() : AppCompatActivity() {
+class RegisterActivity() : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var imageIntentLauncher : ActivityResultLauncher<Intent>
     var member = MemberModel()
     lateinit var app: MainApp
+    private lateinit var auth: FirebaseAuth
+    private lateinit var togglePasswordVisButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,7 +65,7 @@ class RegisterActivity() : AppCompatActivity() {
             binding.memberPassword.setText(member.password)
             binding.memberDob.setText(member.dob)
             binding.experienceSpinner.setSelection(experienceOptions.indexOf(member.experience))
-            binding.btnAdd.setText(R.string.update_member)
+            binding.registerButton.setText(R.string.update_member)
             Picasso.get()
                 .load(member.image)
                 .into(binding.memberImage)
@@ -75,44 +81,91 @@ class RegisterActivity() : AppCompatActivity() {
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
-        val togglePasswordVisButton = findViewById<ImageButton>(R.id.togglePasswordVisibility)
+        binding.registerButton.setOnClickListener(this)
+        binding.togglePasswordVisButton.setOnClickListener(this)
+        binding.chooseImage.setOnClickListener(this)
+
+//        binding.registerButton.setOnClickListener() {
+//            member.firstName = binding.memberFirstName.text.toString()
+//            member.lastName = binding.memberLastName.text.toString()
+//            member.email = binding.memberEmail.text.toString()
+//            member.password = binding.memberPassword.text.toString()
+//            member.dob = binding.memberDob.text.toString()
+//            if (member.firstName.isEmpty() || member.lastName.isEmpty()
+//                || member.dob.isEmpty() || member.experience == selectExperienceString
+//                || member.email.isEmpty() || member.password.isEmpty()) {
+//                Snackbar
+//                    .make(it, R.string.fill_in_all_fields, Snackbar.LENGTH_LONG)
+//                    .show()
+//            } else {
+//                if (edit) {
+//                    app.members.update(member.copy())
+//                } else {
+//                    app.members.create(member.copy())
+//                }
+//                setResult(RESULT_OK)
+//                finish()
+//            }
+//        }
+
+        togglePasswordVisButton = findViewById(R.id.togglePasswordVisButton)
         togglePasswordVisButton.setImageResource(R.drawable.ic_eye)
 
-        binding.togglePasswordVisibility.setOnClickListener() {
-            val passwordEntry = findViewById<EditText>(R.id.memberPassword)
-            if(togglePasswordVisButton.drawable.constantState == ContextCompat.getDrawable(this, R.drawable.ic_eye)?.constantState) {
-                togglePasswordVisButton.setImageResource(R.drawable.ic_eye_slash)
-                passwordEntry.transformationMethod = HideReturnsTransformationMethod.getInstance()
-            } else {
-                togglePasswordVisButton.setImageResource(R.drawable.ic_eye)
-                passwordEntry.transformationMethod = PasswordTransformationMethod.getInstance()
-            }
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Timber.d( "createAccount:$email")
+        if (!validateForm()) {
+            return
         }
 
-        binding.btnAdd.setOnClickListener() {
-            member.firstName = binding.memberFirstName.text.toString()
-            member.lastName = binding.memberLastName.text.toString()
-            member.email = binding.memberEmail.text.toString()
-            member.password = binding.memberPassword.text.toString()
-            member.dob = binding.memberDob.text.toString()
-            if (member.firstName.isEmpty() || member.lastName.isEmpty()
-                || member.dob.isEmpty() || member.experience == selectExperienceString
-                || member.email.isEmpty() || member.password.isEmpty()) {
-                Snackbar
-                    .make(it, R.string.fill_in_all_fields, Snackbar.LENGTH_LONG)
-                    .show()
-            } else {
-                if (edit) {
-                    app.members.update(member.copy())
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Timber.d( "createUserWithEmail:success")
+                    finish()
+                    setResult(RESULT_OK)
                 } else {
-                    app.members.create(member.copy())
+                    // If sign in fails, display a message to the user.
+                    Timber.w( "createUserWithEmail:failure $task.exception")
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
                 }
-                setResult(RESULT_OK)
-                finish()
             }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = binding.memberEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.memberEmail.error = "Required."
+            valid = false
+        } else {
+            binding.memberEmail.error = null
         }
-        binding.chooseImage.setOnClickListener {
-            showImagePicker(imageIntentLauncher)
+
+        val password = binding.memberPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.memberPassword.error = "Required."
+            valid = false
+        } else {
+            binding.memberPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun togglePasswordVisibility() {
+        val passwordEntry = findViewById<EditText>(R.id.memberPassword)
+        if(togglePasswordVisButton.drawable.constantState == ContextCompat.getDrawable(this, R.drawable.ic_eye)?.constantState) {
+            togglePasswordVisButton.setImageResource(R.drawable.ic_eye_slash)
+            passwordEntry.transformationMethod = HideReturnsTransformationMethod.getInstance()
+        } else {
+            togglePasswordVisButton.setImageResource(R.drawable.ic_eye)
+            passwordEntry.transformationMethod = PasswordTransformationMethod.getInstance()
         }
     }
 
@@ -148,5 +201,16 @@ class RegisterActivity() : AppCompatActivity() {
                     RESULT_CANCELED -> { } else -> { }
                 }
             }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.registerButton -> createAccount(
+                binding.memberEmail.text.toString(),
+                binding.memberPassword.text.toString()
+            )
+            R.id.chooseImage -> showImagePicker(imageIntentLauncher)
+            R.id.togglePasswordVisButton -> togglePasswordVisibility()
+        }
     }
 }
